5ba3b0d2685bad2553db47ffdb80f9966df718c5
VuAnhTuan1805 <tuanmeh123@gmail.com>
2026-07-19 15:08:04 +0700

    fix: move to net8

--- services/backend/src/Api/Program.cs
+++ services/backend/src/Api/Program.cs
@@ -6,7 +6,7 @@ var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddInfrastructure(builder.Configuration);
 
-builder.Services.AddOpenApi();
+builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
 
